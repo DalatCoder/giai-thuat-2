ef72324cf22a9514617c22a60eb838bfe11eae28
DalatCoder <hieuntctk42@gmail.com>
2020-01-11 14:55:25 +0700

    Duyet do thi DFS de quy

---
+++ Lab04_1812756/Bai03/Bai03/program.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+#include "common.h"
+#include "graph.h"
+
+int main()
+{
+	Graph g;
+	OpenGraph(g, "TextA.txt");
+	cout << "\nDoc xong do thi A" << endl;
+	DFS_Recursion(g, 0);
+	cout << endl;
+
+	OpenGraph(g, "TextB.txt");
+	cout << "\nDoc xong do thi B" << endl;
+	DFS_Recursion(g, 0);
+	cout << endl;
+
+	OpenGraph(g, "TextC.txt");
+	cout << "\nDoc xong do thi C" << endl;
+	DFS_Recursion(g, 0);
+	cout << endl;
+
+	OpenGraph(g, "TextD.txt");
+	cout << "\nDoc xong do thi D" << endl;
+	DFS_Recursion(g, 0);
+	cout << endl;
+
+	OpenGraph(g, "TextE.txt");
+	cout << "\nDoc xong do thi E" << endl;
+	DFS_Recursion(g, 0);
+	cout << endl;
+
+	OpenGraph(g, "TextF.txt");
+	cout << "\nDoc xong do thi F" << endl;
+	DFS_Recursion(g, 0);
+	cout << endl;
+
+	system("Pause");
+	return 0;
+}
