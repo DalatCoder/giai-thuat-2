b9161a9282c70ec68cdd7f4cc22ea8b345e1de8e
DalatCoder <hieuntctk42@gmail.com>
2020-01-11 14:36:29 +0700

    Kiem tra ham mo do thi tu tap tin

--- Lab04_1812756/Bai02/Bai02/program.cpp
+++ Lab04_1812756/Bai02/Bai02/program.cpp
@@ -10,6 +10,25 @@ using namespace std;
 
 int main()
 {
-	cout << "\nHello world!";
+	Graph g;
+	OpenGraph(g, "TextA.txt");
+	cout << "\nDoc xong do thi A" << endl;
+
+	OpenGraph(g, "TextB.txt");
+	cout << "\nDoc xong do thi B" << endl;
+
+	OpenGraph(g, "TextC.txt");
+	cout << "\nDoc xong do thi C" << endl;
+
+	OpenGraph(g, "TextD.txt");
+	cout << "\nDoc xong do thi D" << endl;
+
+	OpenGraph(g, "TextE.txt");
+	cout << "\nDoc xong do thi E" << endl;
+
+	OpenGraph(g, "TextF.txt");
+	cout << "\nDoc xong do thi F" << endl;
+
+	system("Pause");
 	return 0;
 }
